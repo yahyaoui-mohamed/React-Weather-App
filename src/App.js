1b3f5b87fbdd58a4d2b7a19f127b32e31564c78a
medyah999 <51438000+medyah999@users.noreply.github.com>
2021-11-09 23:47:17 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from "react";
 import './App.css';
-// import axios from "axios";
-
+import broken_clouds from "./assets/broken_clouds.png"
+import clear_sky from "./assets/clear_sky.png"
+import few_clouds from "./assets/few_clouds.png"
 const API_KEY = "1e7621016802c50c28adea8425ac5d69";
 class App extends Component {
   state = {
     temp: "",
     weather: "",
     humidity: "",
-    cityName: ""
+    cityName: "",
+    notFound: true,
+    notSet: true,
+    main: ""
   }
 
 
   showWeather = async (e) => {
     if (e.key === "Enter") {
       let input = document.getElementById("input");
+      if (input.value === "")
+        return;
       const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${API_KEY}`);
       const data = await api.json();
-      this.setState({
-        temp: data.main.temp,
-        weather: data.weather[0].description,
-        humidity: data.main.humidity,
-        cityName: input.value
-      })
+
+      if (api.status === 404) {
+        this.setState({
+          temp: "",
+          weather: "",
+          humidity: "",
+          cityName: "",
+          notFound: true,
+          notSet: false,
+          main: data.weather[0].description
+        })
+      }
+      else {
+        this.setState({
+          temp: (data.main.temp - 273).toFixed(2) + "°C",
+          weather: data.weather[0].description,
+          humidity: data.main.humidity + "%",
+          cityName: input.value[0].toUpperCase() + input.value.substr(1),
+          notFound: false,
+          notSet: false
+        })
+      }
+      console.log(data);
     }
 
   }
@@ -30,16 +53,20 @@ class App extends Component {
     return (
       <div className="app" >
         <input placeholder="Enter name of city ex: Tunis" onKeyUp={this.showWeather} id="input" autoComplete="false" />
-        {this.state.cityName ?
-          (
-            <div className="weather">
-              <p>Temparature in {this.state.cityName}</p>
-              < p >Temperature : {(Number(this.state.temp) - 273).toFixed(2)}</p>
-              <p>Weather : {this.state.weather}</p>
-              <p>Humidity : {this.state.humidity}%</p>
-            </div>)
+        {!this.state.notFound && !this.state.notSet ?
+          <div className="weather">
+            <h1>{this.state.cityName}</h1>
+            <h2>{this.state.temp}</h2>
+            <p>{this.state.weather}</p>
+            <p>{this.state.humidity}</p>
+            <img src={
+              this.state.main === "clear sky" ? "./assets/clear_sky.png" :
+                this.state.main === "broken_clouds" ? broken_clouds : ""
 
-          : ""}
+
+            }></img>
+          </div> : this.state.notFound && !this.state.notSet ? <div>Invalid City Name.</div> : ""
+        }
       </div>
     )
   }
