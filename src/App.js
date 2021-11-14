5231ea9723466a79c6d6a8b5798fa8dba9c8a7ec
medyah999 <51438000+medyah999@users.noreply.github.com>
2021-11-14 16:12:54 +0200

    Switch to Hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import './App.css';
-
-// Background Images Imports
 import overcast_clouds from "./assets/overcast_clouds.jpg";
 import clear_sky from "./assets/clear_sky.jpg";
 import broken_clouds from "./assets/broken_clouds.jpg";
 
+function App() {
+  const API_KEY = "1e7621016802c50c28adea8425ac5d69";
+  const [temp, setTemp] = useState("");
+  const [weather, setWeather] = useState("");
+  const [cityName, setCityName] = useState("");
+  const [notFound, setNotFound] = useState(true);
+  const [notSet, setNotSet] = useState(true);
+  const [country, setCountry] = useState("");
 
-
-
-const API_KEY = "1e7621016802c50c28adea8425ac5d69";
-class App extends Component {
-  state = {
-    temp: "",
-    weather: "",
-    humidity: "",
-    cityName: "",
-    notFound: true,
-    notSet: true,
-    main: ""
-  }
-
-
-  showWeather = async (e) => {
+  async function showWeather(e) {
     if (e.key === "Enter") {
       let input = document.getElementById("input");
       if (input.value === "")
@@ -31,54 +22,42 @@ class App extends Component {
       const data = await api.json();
 
       if (api.status === 404) {
-        this.setState({
-          temp: "",
-          weather: "",
-          humidity: "",
-          cityName: "",
-          notFound: true,
-          notSet: false,
-          main: ""
-        })
+        setNotFound(true);
+        setNotSet(false);
+        return;
       }
       else {
-        this.setState({
-          temp: Math.ceil((data.main.temp - 273).toFixed(2)) + "°C",
-          weather: data.weather[0].description,
-          humidity: data.main.humidity + "%",
-          cityName: input.value[0].toUpperCase() + input.value.substr(1),
-          notFound: false,
-          notSet: false,
-          country: data.sys.country
-        })
+        setTemp(Math.ceil((data.main.temp - 273).toFixed(2)) + "°C");
+        setWeather(data.weather[0].description);
+        setCityName(input.value[0].toUpperCase() + input.value.substr(1));
+        setNotFound(false);
+        setNotSet(false);
+        setCountry(data.sys.country);
       }
     }
-
   }
-  render() {
-    return (
-      <div className="app" style=
-        {this.state.weather === "overcast clouds" ? { backgroundImage: `url(${overcast_clouds})` } :
-          this.state.weather === "clear sky" ? { backgroundImage: `url(${clear_sky})` } :
-            this.state.weather === "broken clouds" ? { backgroundImage: `url(${broken_clouds})` } :
-              { backgroundImage: `url()` }
-        } >
+  return (
+    <div className="app" style=
+      {weather === "overcast clouds" ? { backgroundImage: `url(${overcast_clouds})` } :
+        weather === "clear sky" ? { backgroundImage: `url(${clear_sky})` } :
+          weather === "broken clouds" ? { backgroundImage: `url(${broken_clouds})` } :
+            { backgroundImage: `url()` }
+      } >
 
-        <input placeholder="Enter name of city ex: Tunis" onKeyUp={this.showWeather} id="input" autoComplete="off" />
-        {
-          !this.state.notFound && !this.state.notSet ?
-            <div className="weather">
-              <h1>{this.state.cityName}, {this.state.country}</h1>
-              <div className="temp">
-                <h2>{this.state.temp}</h2>
-              </div>
-              <div className="desc">
-                <p>{this.state.weather}</p>
-              </div>
-            </div> : this.state.notFound && !this.state.notSet ? <div>Invalid City Name.</div> : ""
-        }
-      </ div >
-    )
-  }
+      <input placeholder="Enter name of city ex: Tunis" onKeyUp={showWeather} id="input" autoComplete="off" />
+      {
+        !notFound && !notSet ?
+          <div className="weather">
+            <h1>{cityName}, {country}</h1>
+            <div className="temp">
+              <h2>{temp}</h2>
+            </div>
+            <div className="desc">
+              <p>{weather}</p>
+            </div>
+          </div> : notFound && !notSet ? <div>Invalid City Name.</div> : ""
+      }
+    </ div >
+  )
 }
 export default App;
